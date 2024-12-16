45cde79e4145e263f1d4c60cd4cac1a57babbe32
santoshhalke <halkesantosh1577@gmail.com>
2024-12-16 23:51:46 +0530

    not initial yet initial

---
+++ gfg-rscoe/app/components/hero-section.tsx
@@ -0,0 +1,32 @@
+export default function HeroSection(){
+    return(
+        <div className="header-bar">
+            <div className="logo-img">
+                <img src="#" alt="logo here" />
+            </div>
+            <div className="nav-bar">
+                <ul>
+                    <li>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#">EVENTS</a>
+                    </li>
+                    <li>
+                        <a href="#">OUR TEAM</a>
+                    </li>
+                    <li>
+                        <a href="#">ABOUT US</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="community-button">
+                <button type="submit">Join Our Community</button>
+            </div>
+        </div>
+        // <div className="hero-section">
+        //     <h1>Hero section here</h1>
+        // </div>
+    );
+}
---
+++ gfg-rscoe/app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from './components/hero-section';
+import './css/hero.css';
+import './css/page.css';
+
+
+export default function Home() {
+  return (
+    <div className='main-container'>
+      <HeroSection />
+    </div>
+  );
+}
